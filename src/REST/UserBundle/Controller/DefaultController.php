eab8b00bf1f73c6380e71cc35afe6b791cebe6f1
mouradevsoltani <mouradevsoltani@gmail.com>
2016-10-03 12:22:45 +0100

    Affichage des articles dans l'index avec REST et angular js

---
+++ src/REST/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace REST\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+}
